db59309058c633590c149c55cf31fac9cc543a7b
Hung-Wei Liu <hungwei@login27.chn.perlmutter.nersc.gov>
2026-01-31 02:21:14 -0800

    file search logic complete. Need to solve the QC problem

--- scripts/prepare_baseline.py
+++ scripts/prepare_baseline.py
@@ -5,12 +5,12 @@ from xtc_utils import files_and_chnid, get_baseline_energy
 REPO_ROOT = Path(__file__).resolve().parents[1]
 
 CONFIG_PATH = REPO_ROOT / "xtc_config.json"
-data_dict = {"p08": ["r014","r015"]}
+# data_dict = {"p08": ["r014","r015"]}
 OUTDIR = REPO_ROOT / "temp_results" / "parameters"
 
 OUTDIR.mkdir(parents=True, exist_ok=True)
 
-new_hit_list, new_dsp_list, chn_id = files_and_chnid(CONFIG_PATH, "xtc_old", data_dict)
+new_hit_list, new_dsp_list, chn_id = files_and_chnid(CONFIG_PATH, "xtc_p16")
 
 positive_baseline, negative_baseline, skipped_channels = (
     get_baseline_energy(new_hit_list, new_dsp_list, chn_id)
