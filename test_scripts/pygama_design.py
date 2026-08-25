d8a01c73e53caa00a8ae25f1f999bd1efc02a8e4
hungwei <ken920902@gmail.com>
2026-08-25 08:25:06 +0800

    complete prototype for pygama unit

--- test_scripts/pygama_design.py
+++ test_scripts/pygama_design.py
@@ -1,20 +1,21 @@
 def prepare_baseline(hit_files: str | list, dsp_files: str | list, 
-                     chn_id: str | list, temp_dir: str | Path) -> None: 
+                     chn_id: str | list, out_path: str | Path) -> None: 
     raise NotImplementedError()
 
 # merge baseline? 
 
 def xtalk_column(hit_files: str | list, dsp_files: str | list, 
-                 trigger_detector_id: str, chn_id_list: list, baseline: dict) -> None:
+                 trigger_detector_id: str, chn_id_list: list, 
+                 baseline: dict, out_path: str | Path) -> None:
     raise NotImplementedError()
 
 # Or maybe baseline should be two separate arguments? 
 # Or maybe baseline is a file path?
 
-def xtalk_histogram_fitter(trigger_detector_id: str, histogram_file: str | Path, 
-                           config: dict | None) -> None:
+def xtalk_histogram_fitter(histogram_file: str | Path, config: dict | None, 
+                           out_path: str | Path) -> None:
     raise NotImplementedError()
 
-def build_xtalk_matrix(chn_id_list: list, histogram_files: list, fitted_files: list, 
+def build_xtalk_matrix(chn_id_list: list, fitted_files: list, 
                        out_path: str | Path, config: dict | None) -> None:
     raise NotImplementedError()
